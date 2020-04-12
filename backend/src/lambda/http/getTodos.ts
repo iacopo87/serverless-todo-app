b885619c1afec199b725a2a45a805b76fa4eb141
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2020-04-12 22:29:36 +0200

    refactor: getTodos - use middy for cors

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,27 +1,25 @@
 import 'source-map-support/register'
 import { getAllTodos } from '../../businessLogic/todos'
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  APIGatewayProxyHandler
-} from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
+
 const logger = createLogger('getTodos')
 
-export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  logger.info('Processing event', event)
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Processing event', event)
 
-  const userId = getUserId(event)
-  const items = await getAllTodos(userId)
+    const userId = getUserId(event)
+    const items = await getAllTodos(userId)
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({ items })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ items })
+    }
   }
-}
+)
+
+handler.use(cors())
