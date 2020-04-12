200772a9c1241a27d362cc49316df5319ad17960
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2020-04-12 17:08:12 +0200

    feat: Add Auth0 authorizer

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-et3e3dbt.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -57,11 +54,9 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const pem = await getPEM(jwt)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return verify(token, pem) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +69,33 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getPEM(jwt): Promise<string> {
+  const certkeys = await Axios.get(jwksUrl)
+  const signingKey = certkeys.data.keys
+    .filter(
+      (key) =>
+        key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+        key.kty === 'RSA' && // We are only supporting RSA (RS256)
+        key.kid && // The `kid` must be present to be useful for later
+        ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+    )
+    .map((key) => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) }
+    })
+    .find((keys) => keys.kid == jwt.header.kid)
+
+  if (!signingKey) {
+    throw new Error('Unknown jwt token')
+  }
+
+  return signingKey.publicKey
+}
+
+// https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/master/src/lib/utils.js
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'lmg4jeq827'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-et3e3dbt.eu.auth0.com',
+  clientId: 'PkU3DuU52JaUcr2zLKEBsz8a6m6aLgd4',
   callbackUrl: 'http://localhost:3000/callback'
 }
